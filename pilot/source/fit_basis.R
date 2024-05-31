34cfe6b2fb1079b4bc15206178fa7d0dddd29fd3
Bao Anh Vu <vubaoanhh@gmail.com>
2024-05-31 19:16:11 +1000

    Added NN with both bed and friction as output

--- pilot/source/fit_basis.R
+++ pilot/source/fit_basis.R
@@ -1,4 +1,4 @@
-fit_basis <- function(nbasis, domain, sample_arr) {
+fit_friction_basis <- function(nbasis, domain, sample_arr) {
     ## Place basis function centres along domain
     # basis_centres <- seq(domain[1], domain[length(domain)], length.out = nbasis+2)
     # basis_centres <- basis_centres[2:(length(basis_centres)-1)] 
@@ -20,12 +20,53 @@ fit_basis <- function(nbasis, domain, sample_arr) {
     basis_coefs <- matrix(NA, N, nbasis) 
     fitted_values <- matrix(NA, N, length(domain))
     for (sim in 1:N) { # parallelise
-        df_local <- as.data.frame(cbind(sample_arr[sim,,,], basis_mat))
+        df_local <- as.data.frame(cbind(sample_arr[sim, ], basis_mat))
         colnames(df_local) <- c("fric", sapply(1:nbasis, function(x) paste0("eof", x)))
         lmfit_local <- lm(fric ~ . - 1, data = df_local) # no intercept for now so -1
         basis_coefs[sim, ] <- as.vector(lmfit_local$coefficients)
         fitted_values[sim, ] <- as.vector(lmfit_local$fitted.values)
     }
+
+    return(list(basis_coefs = basis_coefs, 
+                basis_mat = basis_mat,
+                fitted_values = fitted_values))
+
+}
+
+
+## Basis for the bedrock
+fit_bed_basis <- function(nbasis, domain, sample_arr) {
+    ## Place basis function centres along domain
+    # basis_centres <- seq(domain[1], domain[length(domain)], length.out = nbasis+2)
+    # basis_centres <- basis_centres[2:(length(basis_centres)-1)] 
+                            
+    basis_centres <- seq(domain[1], domain[length(domain)], length.out = nbasis)
+    testbasis <- local_basis(manifold = real_line(), 
+                            loc = matrix(basis_centres),
+                            type = "bisquare",
+                            scale = rep(5e3, nbasis))
+    # show_basis(testbasis)
+    basis_fns <- lapply(testbasis@fn, function(f) f(domain))
+    # plot(domain, basis_fns[[10]])
+    basis_mat <- do.call(cbind, basis_fns)
+    # matplot(domain/1000, basis_mat, type = "l", col= "salmon", lty = 1, lwd = 1.5,
+    #         xlab = "Domain (km)", xlim = c(0, 200))
+
+
+    N <- dim(sample_arr)[1]
+    basis_coefs <- matrix(NA, N, nbasis) 
+    fitted_values <- matrix(NA, N, length(domain))
+    for (sim in 1:N) { # parallelise
+        df_local <- as.data.frame(cbind(sample_arr[sim, ], basis_mat))
+        colnames(df_local) <- c("fric", sapply(1:nbasis, function(x) paste0("eof", x)))
+        lmfit_local <- lm(fric ~ . - 1, data = df_local) # no intercept for now so -1
+        basis_coefs[sim, ] <- as.vector(lmfit_local$coefficients)
+        fitted_values[sim, ] <- as.vector(lmfit_local$fitted.values)
+    }
+
+# plot(sample_arr[1,], type = "l")
+# lines(fitted_values[1, ], col = "red")
+#     browser()
 
     return(list(basis_coefs = basis_coefs, 
                 basis_mat = basis_mat,
--- pilot/source/run_sims.R
+++ pilot/source/run_sims.R
@@ -1,11 +1,10 @@
-run_sims <- function(nsims) {
-
+run_sims <- function(nsims, sim_beds = F, bed_obs) {
     N <- nsims
     years <- 50
     domain <- ssa_steady$domain
 
     secpera <- 31556926
-    fric_scale <- 1e6 * secpera^(1/3)
+    fric_scale <- 1e6 * secpera^(1 / 3)
 
     # 1. Simulate friction coefficients
     print("Simulating friction coefficient...")
@@ -14,11 +13,32 @@ run_sims <- function(nsims) {
     fric.nugget <- 0
     fric.range <- 10e3
 
-    simulated_friction <- simulate_friction2(nsim = N, domain = domain,
-                                            sill = fric.sill, nugget = fric.nugget, 
-                                            range = fric.range) # doesn't need input, or maybe just put the mean in there
+    simulated_friction <- simulate_friction2(
+        nsim = N, domain = domain,
+        sill = fric.sill, nugget = fric.nugget,
+        range = fric.range
+    ) # doesn't need input, or maybe just put the mean in there
 
-    sim_fric_list <- lapply(1:N, function(c) simulated_friction[, c])
+    # sim_fric_list <- lapply(1:N, function(c) simulated_friction[, c])
+
+    ## 1.5. Simulate beds
+    if (sim_beds) {
+        print("Simulating beds...")
+        bed_sims <- simulate_bed(
+            nsim = N, domain = domain,
+            obs_location = bed_obs$locations, obs = bed_obs$obs
+        )
+
+        # sim_bed_list <- lapply(1:N, function(c) bed_sims[, c])
+        sim_param_list <- lapply(1:N, function(c) {
+            list(
+                friction = simulated_friction[, c],
+                bedrock = bed_sims[, c]
+            )
+        })
+    } else {
+        sim_param_list <- lapply(1:N, function(c) list(friction = simulated_friction[, c]))
+    }
 
     ## 2. Simulate ice thickness and velocity observations
     print("Simulating observations...")
@@ -29,56 +49,130 @@ run_sims <- function(nsims) {
     ones <- rep(1, length(domain))
     D <- rdist(domain)
     l <- 50e3
-    R <- outer(ones, ones) * (1 + sqrt(3) * D / l) * exp(- sqrt(3) * D / l )
+    R <- outer(ones, ones) * (1 + sqrt(3) * D / l) * exp(-sqrt(3) * D / l)
     L <- t(chol(R))
     L <- as(L, "dgCMatrix")
     process_noise_info <- list(corrmat_chol = L, length_scale = l)
 
     t1 <- proc.time()
-    sim_results <- mclapply(sim_fric_list, function(simulated_friction) {
-    # cat("sim =", sim, "\n")
-    # steady_state = ssa_steady
-    reference <- solve_ssa_nl(domain = ssa_steady$domain, 
-                                bedrock = ssa_steady$bedrock, 
-                                friction_coef = simulated_friction,
-                                ini_velocity = ssa_steady$current_velocity,
-                                ini_thickness = ssa_steady$current_thickness,
-                                years = years, steps_per_yr = 52, 
-                                save_model_output = TRUE, 
-                                perturb_hardness = TRUE,
-                                add_process_noise = T,
-                                process_noise_info = process_noise_info)    
 
-    thickness_velocity_obs <- array(data = cbind(reference$all_thicknesses[, 2:(years+1)], 
-                                reference$all_velocities[, 2:(years+1)]),
-                                dim = c(length(domain), years, 2))
-    gl <- reference$grounding_line
+    if (sim_beds) {
+        sim_results <- mclapply(sim_param_list, function(sim_param) {
+            # cat("sim =", sim, "\n")
+            # steady_state = ssa_steady
 
-    simulated_data <- list(thickness_velocity_arr = thickness_velocity_obs,
-                            friction_arr = simulated_friction,
-                            grounding_line = gl)
-    # simulated_data <- obs
-    return(simulated_data)
-    },
-    #steady_state = ssa_steady, 
-    mc.cores = 50L)
+            reference <- solve_ssa_nl(
+                domain = ssa_steady$domain,
+                bedrock = sim_param$bedrock, # use true bed
+                friction_coef = sim_param$friction,
+                ini_velocity = ssa_steady$current_velocity,
+                ini_thickness = ssa_steady$current_thickness,
+                years = years, steps_per_yr = 52,
+                save_model_output = TRUE,
+                perturb_hardness = TRUE,
+                add_process_noise = T,
+                process_noise_info = process_noise_info
+            )
+
+            thickness_velocity_obs <- array(
+                data = cbind(
+                    reference$all_thicknesses[, 2:(years + 1)],
+                    reference$all_velocities[, 2:(years + 1)]
+                ),
+                dim = c(length(domain), years, 2)
+            )
+            gl <- reference$grounding_line
+
+            simulated_data <- list(
+                thickness_velocity_arr = thickness_velocity_obs,
+                friction_arr = sim_param$friction,
+                bed_arr = sim_param$bedrock,
+                grounding_line = gl
+            )
+            # simulated_data <- obs
+            return(simulated_data)
+        },
+        # steady_state = ssa_steady,
+        mc.cores = 50L
+        )
+    } else {
+        sim_results <- mclapply(sim_param_list, function(sim_param) {
+            # cat("sim =", sim, "\n")
+            # steady_state = ssa_steady
+
+            reference <- solve_ssa_nl(
+                domain = ssa_steady$domain,
+                bedrock = ssa_steady$bedrock, # use true bed
+                friction_coef = sim_param$friction,
+                ini_velocity = ssa_steady$current_velocity,
+                ini_thickness = ssa_steady$current_thickness,
+                years = years, steps_per_yr = 52,
+                save_model_output = TRUE,
+                perturb_hardness = TRUE,
+                add_process_noise = T,
+                process_noise_info = process_noise_info
+            )
+
+            thickness_velocity_obs <- array(
+                data = cbind(
+                    reference$all_thicknesses[, 2:(years + 1)],
+                    reference$all_velocities[, 2:(years + 1)]
+                ),
+                dim = c(length(domain), years, 2)
+            )
+            gl <- reference$grounding_line
+
+            simulated_data <- list(
+                thickness_velocity_arr = thickness_velocity_obs,
+                friction_arr = sim_param$friction,
+                grounding_line = gl
+            )
+            # simulated_data <- obs
+            return(simulated_data)
+        },
+        # steady_state = ssa_steady,
+        mc.cores = 50L
+        )
+    }
+
+
     t2 <- proc.time()
 
     ## Note to self: should save a version with the "true" friction coef as well
     thickness_velocity_list <- lapply(1:N, function(i) sim_results[[i]]$thickness_velocity_arr)
     friction_list <- lapply(1:N, function(i) sim_results[[i]]$friction_arr)
+    bed_list <- lapply(1:N, function(i) sim_results[[i]]$bed_arr)
     gl_list <- lapply(1:N, function(i) sim_results[[i]]$grounding_line)
 
     concat_input <- do.call("rbind", thickness_velocity_list)
     thickness_velocity_arr <- array(concat_input, dim = c(N, dim(thickness_velocity_list[[1]])))
 
     concat_output <- do.call("rbind", friction_list)
-    friction_arr <- array(concat_output, dim = c(N, length(friction_list[[1]]), 1L, 1L))
+    friction_arr <- array(concat_output, dim = c(N, length(friction_list[[1]])))
+
+    if (sim_beds) {
+        concat_output <- do.call("rbind", bed_list)
+        bed_arr <- array(concat_output, dim = c(N, length(bed_list[[1]])))
+    }
 
     concat_gl <- do.call("rbind", gl_list)
-    gl_arr <- array(concat_gl, dim = c(N, 1L, length(gl_list[[1]]), 1L))
+    gl_arr <- array(concat_gl, dim = c(N, length(gl_list[[1]])))
 
-    return(list(thickness_velocity_arr = thickness_velocity_arr, 
-                friction_arr = friction_arr, 
-                gl_arr = gl_arr))
+    if (sim_beds) {
+        return_obj <- list(
+            thickness_velocity_arr = thickness_velocity_arr,
+            friction_arr = friction_arr,
+            bed_arr = bed_arr,
+            gl_arr = gl_arr
+        )
+    } else {
+        return_obj <- list(
+            thickness_velocity_arr = thickness_velocity_arr,
+            friction_arr = friction_arr,
+            gl_arr = gl_arr
+        )
+    
+    }
+
+    return(return_obj)
 }
