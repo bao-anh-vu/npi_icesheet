96247f26743348aeb4baa0cc9153cd5d9772d5eb
Bao Anh Vu <vubaoanhh@gmail.com>
2025-10-03 11:42:47 +1000

    Cleaned old comments from script

--- real_data/08_run_cnn_posterior.R
+++ real_data/08_run_cnn_posterior.R
@@ -56,28 +56,15 @@ sets <- 1:10 #c(1,3,5) #11:15 #6:10 #arg
 setsf <- paste0("sets", sets[1], "-", sets[length(sets)])
 
 print("Reading data...")
-# if (use_missing_pattern) {
   train_data_dir <- paste0("./data/training_data", "/", setsf, "/")
   output_dir <- paste0("./output/cnn/", setsf, "/")
   plot_dir <- paste0("./plots/cnn/", setsf, "/")
-# } else {
-#   train_data_dir <- paste0("./data/training_data", "/", setsf, "/nonmissing")
-#   plot_dir <- paste0("./plots/cnn/", setsf, "/nonmissing/")
-# }
 
-# train_data <- readRDS(file = paste0(train_data_dir, "/train_data_", data_date, ".rds"))
-#   val_data <- readRDS(file = paste0(train_data_dir, "/val_data_", data_date, ".rds"))
-#   test_data <- readRDS(file = paste0(train_data_dir, "/test_data_", data_date, ".rds"))
 system.time({
   train_data <- qread(file = paste0(train_data_dir, "train_data_", data_date, ".qs"))
   val_data <- qread(file = paste0(train_data_dir, "val_data_", data_date, ".qs"))
   test_data <- qread(file = paste0(train_data_dir, "test_data_", data_date, ".qs"))
 })
-# system.time({
-  # qsave(train_data, file = paste0(train_data_dir, "/train_data_", data_date, ".qs"))
-  # qsave(val_data, file = paste0(train_data_dir, "/val_data_", data_date, ".qs"))
-  # qsave(test_data, file = paste0(train_data_dir, "/test_data_", data_date, ".qs"))
-# })
 
 train_input <- train_data$input
 val_input <- val_data$input
@@ -105,11 +92,9 @@ lines(val_output[sp, ], col = scales::alpha("red", 0.5))
 legend("topright", legend = c("train", "val"), col = c("grey", "red"), lty = 1)
 dev.off()
 
-# train_output <- train_data$output
-# val_output <- val_data$output
-# test_output <- test_data$output
-
-# if (rerun_cnn) {
+############################
+##      Create model      ##
+############################
 print("Creating model...")
 # Create a basic model instance
 input_dim <- dim(train_data$input)[2:4]
@@ -120,28 +105,11 @@ n_mean_elements <- n_fric_basis + n_bed_basis + n_gl
 n_chol_elements <- n_mean_elements + (n_mean_elements - 3) # diagonal + lower-diag elements
 output_dim <- n_mean_elements + n_chol_elements  # THIS NEEDS TO CHANGE TO THE TOTAL NUMBER OF BASIS FUNCTIONS + COVARIANCE PARAMETERS
 
-# input_dim <- dim(train_data$input)[2:4]
-# n_bed_basis <- dim(train_data$bed_coefs)[2]
-# n_fric_basis <- dim(train_data$fric_coefs)[2]
-# n_gl <- dim(train_data$grounding_line)[2]
-# n_mean_elements <- n_fric_basis + n_bed_basis + n_gl
-# n_chol_elements <- n_mean_elements + (n_mean_elements - 3) # diagonal + lower-diag elements
-# output_dim <- n_mean_elements + n_chol_elements  # THIS NEEDS TO CHANGE TO THE TOTAL NUMBER OF BASIS FUNCTIONS + COVARIANCE PARAMETERS
-
-
-# if (output_var == "friction") {
-#   model <- create_model(input_dim = input_dim, output_dim = output_dim)
-# } else if (output_var == "grounding_line") {
-#   model <- create_model(input_dim = input_dim, output_dim = output_dim)
-# } else if (output_var == "bed") { ## bed
-#   model <- create_model_bed(input_dim = input_dim, output_dim = output_dim)
-# } else { ## all variables
   model <- create_model_posterior(input_dim = input_dim, 
                                   output_dim = output_dim,
                                   n_bed_basis = n_bed_basis,
                                   n_fric_basis = n_fric_basis,
                                   n_gl = n_gl)
-# }
 
 # Display the model's architecture
 summary(model)
@@ -154,12 +122,6 @@ val_metrics <- model %>% evaluate(val_input, val_output, verbose = 0)
 
 cat("Initial training loss:", train_metrics["loss"], "\n")
 cat("Initial validation loss:", val_metrics["loss"], "\n")
-
-# Create a callback that saves the model's weights
-# if (use_missing_pattern) {
-# } else {
-#   output_dir <- paste0("./output/cnn/", setsf, "/nonmissing")
-# }
 
 if (!dir.exists(output_dir)) {
   dir.create(paste0(output_dir))
@@ -205,26 +167,9 @@ if (rerun_cnn) {
   history <- qread(file = paste0(output_dir, "/history_", data_date, ".qs"))
 }
 
-# loss_plot <- history %>%
-#   plot() +
-#   coord_cartesian(xlim = c(1, epochs))
-  
-# # ## Plot the loss
-# png(paste0("./plots/cnn/", setsf, "/loss_", data_date, ".png"))
-# print(loss_plot)
-# dev.off()
-
-
-
 if (!dir.exists(plot_dir)) {
     dir.create(plot_dir)
 }
-
-# png(paste0(plot_dir, "loss.png"), width = 1000, height = 500)
-# plot(history$metrics$loss, type = "l")
-# lines(history$metrics$val_loss, col = "red")
-# legend("topright", legend = c("Training", "Validation"), col = c("black", "red"), lty = 1, cex = 0.8)
-# dev.off()
 
 ## Plot the loss
 loss_plot <- history %>%
